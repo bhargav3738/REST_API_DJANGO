c4dad135c37c385d5002df5cc3c4ec6ebb1e2a73
Bhargav Nimbalkar <nimbalkarbhargav19@gmail.com>
2022-08-18 18:33:12 +0530

    first commit

---
+++ nsApp/views.py
@@ -0,0 +1,21 @@
+from .models import Author,Book
+from nsApp.serializers import AuthorSerializer,BookSerializer
+from rest_framework import generics
+
+
+# Create your views here.
+class AuthorListView(generics.ListCreateAPIView):
+    queryset = Author.objects.all()
+    serializer_class = AuthorSerializer
+
+class AuthorDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Author.objects.all()
+    serializer_class = AuthorSerializer
+
+class BookListView(generics.ListCreateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+class BookDetailView(generics.RetrieveUpdateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
